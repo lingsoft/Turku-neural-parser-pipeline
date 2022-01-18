2876982a885cec0ab0b89ae51014017451437332
Hai Lee <duonghailee@gmail.com>
2022-01-18 12:45:45 +0200

    Update docker files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-exec /sbin/tini -- venv/bin/gunicorn --bind=0.0.0.0:8000 "--workers=$WORKERS" "--timeout=$TIMEOUT" "--worker-class=$WORKER_CLASS" --worker-tmp-dir=/dev/shm "$@" tnpp_serve_elg:app
+# now fetch the model
+# /sbin/tini -s -- venv/bin/python fetch_models.py $MODEL && 
+exec /sbin/tini  -- venv/bin/gunicorn --bind=0.0.0.0:8000 "--workers=$WORKERS" "--timeout=$TIMEOUT" "--worker-class=$WORKER_CLASS" --worker-tmp-dir=/dev/shm "$@" tnpp_serve_elg:app
