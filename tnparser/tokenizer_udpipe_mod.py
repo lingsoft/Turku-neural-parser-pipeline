9259d8f3b5f265700d8cde0418d4363ec1a7cedb
Jarmo Hemminki <87070956+jarmo-lls@users.noreply.github.com>
2022-07-11 09:25:34 +0300

    Add option ranges to tokenizer and polish code

--- tnparser/tokenizer_udpipe_mod.py
+++ tnparser/tokenizer_udpipe_mod.py
@@ -8,13 +8,13 @@ import os
 try:
     import ufal.udpipe as udpipe
 except:
-    print("pip3 install ufal.udpipe",file=sys.stderr)
+    print("pip3 install ufal.udpipe", file=sys.stderr)
     raise
 
-comment_regex=re.compile("^####?C: ?")
+comment_regex = re.compile("^####?C: ?")
+
 
 class UDPipeTokenizerWrapper():
-
     def __init__(self, args):
         """
         Tokenizer model loading etc goes here
@@ -24,37 +24,51 @@ class UDPipeTokenizerWrapper():
             sys.exit(-1)
         self.model = udpipe.Model.load(args.model)
         if args.presegmented:
-            self.pipeline = udpipe.Pipeline(self.model,"tokenizer=presegmented","none","none","conllu")
+            self.pipeline = udpipe.Pipeline(self.model,
+                                            "tokenizer=presegmented", "none",
+                                            "none", "conllu")
         else:
-            self.pipeline = udpipe.Pipeline(self.model,"tokenize","none","none","conllu")
-            self.pipeline.setImmediate(True) # the input is processed after loading an empty line
-            
-    def parse_text(self,txt):
-        err=udpipe.ProcessingError()
-        tokenized=""
-        current_block=[]
+            self.pipeline = udpipe.Pipeline(self.model, "tokenizer=ranges",
+                                            "none", "none", "conllu")
+            self.pipeline.setImmediate(
+                True)  # the input is processed after loading an empty line
+
+    def parse_text(self, txt):
+        err = udpipe.ProcessingError()
+        tokenized = ""
+        current_block = []
         for line in txt.split("\n"):
-            if re.match(comment_regex, line.lstrip()): # comment line
+            if re.match(comment_regex, line.lstrip()):  # comment line
                 if current_block:
-                    tokenized+=self.pipeline.process("\n".join(current_block),err)
-                    current_block=[]
-                tokenized+=line.lstrip()+"\n"#re.sub(comment_regex, "# ", line.lstrip()+"\n")
+                    tokenized += self.pipeline.process(
+                        "\n".join(current_block), err)
+                    current_block = []
+                tokenized += line.lstrip(
+                ) + "\n"  #re.sub(comment_regex, "# ", line.lstrip()+"\n")
                 continue
             # normal text line, save to current block to be tokenized
             current_block.append(line)
         if current_block:
-            tokenized+=self.pipeline.process("\n".join(current_block),err)
+            tokenized += self.pipeline.process("\n".join(current_block), err)
         return tokenized
-    
-def launch(args,q_in,q_out):
-    t=UDPipeTokenizerWrapper(args)
+
+
+def launch(args, q_in, q_out):
+    t = UDPipeTokenizerWrapper(args)
     while True:
-        jobid,txt=q_in.get()
-        if jobid=="FINAL":
-            q_out.put((jobid,txt))
+        jobid, txt = q_in.get()
+        if jobid == "FINAL":
+            q_out.put((jobid, txt))
             return
-        q_out.put((jobid,t.parse_text(txt)))
-    
+        q_out.put((jobid, t.parse_text(txt)))
+
+
 argparser = argparse.ArgumentParser(description='UDPipe tokenize text')
 argparser.add_argument("--model", default="model", help="model file")
-argparser.add_argument("--presegmented", default=False, action="store_true", help="Input is already sentence segmented (one sentence per line), run only tokenizer.")
+argparser.add_argument(
+    "--presegmented",
+    default=False,
+    action="store_true",
+    help=
+    "Input is already sentence segmented (one sentence per line), run only tokenizer."
+)
